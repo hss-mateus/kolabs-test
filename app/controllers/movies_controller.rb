137039667152a6e93fe8ae6301957332e6c41d97
hss-mateus <hss-mateus@tuta.io>
2020-10-09 19:27:40 -0300

    Implement remaining movie CRUD operations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :movie_by_id_param, only: %i[edit update destroy]
+
   def index
     @movies = Movie.all
   end
@@ -8,11 +10,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = params
-      .require(:movie)
-      .permit(:title, :release_date, :description, :poster, :score, :genres)
-
-    @movie = Movie.new(movie)
+    @movie = Movie.new(movie_params)
 
     if @movie.save
       redirect_to movies_url
@@ -24,6 +22,29 @@ class MoviesController < ApplicationController
   def edit
   end
 
+  def update
+    if @movie.update(movie_params)
+      redirect_to movies_url
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
+    @movie.destroy
+
+    redirect_to movies_url
+  end
+
+  private
+
+  def movie_params
+    params
+      .require(:movie)
+      .permit(:title, :release_date, :description, :poster, :score, :genres)
+  end
+
+  def movie_by_id_param
+    @movie = Movie.find_by_id(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'search#index'
-  resources :movies
+  resources :movies, only: %i[index new create edit update destroy]
 end
