c2e2788cc065d935cd5aaa6c92fdb364a64cf213
hss-mateus <hss-mateus@tuta.io>
2020-10-09 19:00:14 -0300

    Implement movie creation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,20 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    movie = params
+      .require(:movie)
+      .permit(:title, :release_date, :description, :poster, :score, :genres)
+
+    @movie = Movie.new(movie)
+
+    if @movie.save
+      redirect_to movies_url
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+  validates :title,
+    :release_date,
+    :description,
+    :poster,
+    :score,
+    :genres,
+    presence: true
+
+  validates :title, :poster, uniqueness: { case_sensitive: false }
 end
