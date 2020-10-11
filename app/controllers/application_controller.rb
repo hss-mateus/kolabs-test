f0dd5c5eec44c49671c2a093a160576875c91aa5
hss-mateus <hss-mateus@tuta.io>
2020-10-11 20:32:53 -0300

    Refactor footer sections exibition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :footer_sections
+
+  private
+
+  def footer_sections
+    @footer_sections = [
+      { title: 'The Basics', items: ['About TMDb', 'Contact Us', 'Support Forums', 'API', 'System Status'] },
+      { title: 'Get Involved', items: ['Contibution Bible', '3rd Party Applications', 'Add New Movie', 'Add New TV Show'] },
+      { title: 'Community', items: ['Guidelines', 'Discussions', 'Leaderboard', 'Twitter'] },
+      { title: 'Legal', items: ['Terms of Use', 'API Terms of Use', 'Privacy Policy'] }
+    ]
+  end
 end
